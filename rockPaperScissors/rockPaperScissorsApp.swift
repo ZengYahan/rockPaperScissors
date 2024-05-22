5abbd4b157b4a49428636d33096a32f957603cbf
Han <g43424140@gmail.com>
2024-05-22 23:04:36 +0800

    Initial Commit

---
+++ rockPaperScissors/rockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorsApp.swift
+//  rockPaperScissors
+//
+//  Created by YaHan on 2024/5/22.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
